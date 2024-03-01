b83a6692aea825d833aeadb51322773cf57085bd
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-03-01 17:30:00 +0100

    connection avec la base de donnée PizzaTimeDB et ajout  d'un dossier 'auth' pour l'authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { env } from 'process';
+
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: "postgres",
+      password: "04940043",
+      database: "PizzaTimeDB",
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
 })
+
+
+
+
 export class AppModule {}
