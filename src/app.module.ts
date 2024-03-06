282fb8b300d638df79c9394859bdd2a04d15bb0a
Bouraoui abdallah <ingbouraouiabdallah@gmail.com>
2024-03-06 16:15:29 +0100

    modification dans   le main pour connecter le back avec le front , suppresion de unused importation , installation du core

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { env } from 'process';
 import { UsersModule } from './users/users.module';
 
 
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  }); 
+
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('PIZZA-TIME')
+  .setDescription('PIZZA-TIME API description')
   .setVersion('1.0')
   .addTag('cats')
   .build();
+  app.setGlobalPrefix('api');
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
 
